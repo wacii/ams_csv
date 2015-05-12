03271d8910c2f39f63a3236a7c534ba23ed39d26
William Cunningham <w.a.cunningham.ii@gmail.com>
2015-05-12 10:39:30 -0400

    add spec regarding split attribute declarations

--- spec/model/attributes_spec.rb
+++ spec/model/attributes_spec.rb
@@ -13,12 +13,17 @@ describe 'attributes' do
     attributes :name, :body
   end
 
-  class PostPlusSerializer < PostSerializer
+  class Post2Serializer < PostSerializer
     attributes :name, :body
 
     def name
       'pie'
     end
+  end
+
+  class Post3Serializer < ActiveModel::CsvSerializer
+    attributes :name
+    attributes :body
   end
 
   it 'pulls attributes off associated model' do
@@ -32,11 +37,20 @@ describe 'attributes' do
 
   it 'favors methods defined on serializer' do
     post = Post.new(name: 'Samwise', body: 'Hobbit extraordinaire.')
-    serializer = PostPlusSerializer.new(post)
+    serializer = Post2Serializer.new(post)
     csv = serializer.to_csv
 
     expect(csv).to include('pie')
     expect(csv).to_not include(post.name)
+    expect(csv).to include(post.body)
+  end
+
+  it 'allows attributes declaration to be split up' do
+    post = Post.new(name: 'Samwise', body: 'Hobbit extraordinaire.')
+    serializer = Post3Serializer.new(post)
+    csv = serializer.to_csv
+
+    expect(csv).to include(post.name)
     expect(csv).to include(post.body)
   end
 end
