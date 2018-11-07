fb88b3aa0df51ddaef651aaea9547dc2d830635f
Yann Bolliger <ybstudio@windowslive.com>
2018-11-08 00:47:21 +0100

    Add tests and fixes for array serializer. Issue #1.

--- lib/active_model/csv_array_serializer.rb
+++ lib/active_model/csv_array_serializer.rb
@@ -5,26 +5,35 @@ module ActiveModel
   class CsvArraySerializer
     def initialize(objects, options = {})
       @each_serializer = options[:each_serializer]
+      @root = options[:root].present? ? options[:root] : true
       @objects = objects
       @options = options
     end
 
     def to_a
-      return ActiveModel::CsvSerializer.new(nil).to_a if @objects.nil?
+      return ActiveModel::CsvSerializer.new(nil).to_a unless @objects
       @objects.collect do |object|
-        serializer = @each_serializer || ActiveModel::CsvSerializerFactory
-        serializer.new(object, @options).to_a
+        serializer.new(object, @options).to_a.flatten
       end
     end
 
     def to_csv
-      to_a.to_csv
+      rows = to_a.collect(&:to_csv).join
+      if @root
+        attribute_names.to_csv << rows
+      else
+        rows
+      end
     end
 
     def attribute_names
       return [] unless @objects
-      serializer = @each_serializer || ActiveModel::CsvSerializerFactory
       serializer.new(@objects.first, @options).attribute_names
+    end
+
+    private
+    def serializer
+      @each_serializer || ActiveModel::CsvSerializerFactory
     end
   end
 end
--- spec/active_model/array_serializer_spec.rb
+++ spec/active_model/array_serializer_spec.rb
@@ -6,23 +6,46 @@ describe 'ArraySerializer' do
   let(:comment_2) { Comment.new(text: 'b') }
   let(:post) { Post.new(name: 'c', body: 'd') }
 
-  it 'renders csv using specified serializer' do
-    array_serializer = ActiveModel::CsvArraySerializer.new(
-      comments,
-      each_serializer: CommentCsvSerializer
-    )
-    csv = array_serializer.to_csv
 
-    expect(csv).to include(comment_1.text)
-    expect(csv).to include(comment_2.text)
+  context 'with specified serializer' do
+    subject {
+      ActiveModel::CsvArraySerializer.new(
+        comments,
+        each_serializer: CommentCsvSerializer
+      ).to_csv
+    }
+
+    it 'renders csv and includes headers.' do
+      expect(subject).to include('text')
+    end
+
+    it 'renders csv with the correct number of rows.' do
+      lines = subject.split("\n")
+      expect(lines.size).to eq(3)
+    end
+
+    it 'renders csv with content.' do
+      expect(subject).to include(comment_1.text)
+      expect(subject).to include(comment_2.text)
+    end
   end
 
-  it 'renders csv without specified serializer' do
-    array_serializer = ActiveModel::CsvArraySerializer.new(comments)
-    csv = array_serializer.to_csv
+  context 'without specified serializer' do
+    subject { ActiveModel::CsvArraySerializer.new(comments).to_csv }
 
-    expect(csv).to include(comment_1.text)
-    expect(csv).to include(comment_2.text)
+    it 'renders csv and includes headers.' do
+      expect(subject).to include('text')
+    end
+
+    it 'renders csv with the correct number of rows.' do
+      lines = subject.split("\n")
+      expect(lines.size).to eq(3)
+    end
+
+    it 'renders csv with content.' do
+      expect(subject).to include(comment_1.text)
+      expect(subject).to include(comment_2.text)
+    end
   end
 
   it 'raises an exception when expected serializer not found' do
